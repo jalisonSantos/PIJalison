6ab67156079d69eefed8df55d312b63f41194542
jalisonsantos <jalison2020.sa@gmail.com>
2022-10-24 17:18:23 -0300

    teste

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+
+function HomeScreen({ navigation }) {
+  const irParaSobre = () => {
+    return navigation.navigate("Sobre");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Olá mundo Feliz</Text>
+
+      <Button
+        title="Ir para Sobre"
+        onPress={() => navigation.navigate("Sobre")}
+      />
     </View>
   );
 }
 
+function SobreScreen() {
+  return (
+    <View>
+      <Text>Olá eu sou a página sobre</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={"Home"} component={HomeScreen} />
+
+        <Stack.Screen name={"Sobre"} component={SobreScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
